38d0191cce38674ef7f52982bbe89d537831cb4c
lucbazzani <lu.bazzani@hotmail.com>
2021-10-24 17:46:17 -0300

    User posts endpoint

--- src/main/java/com/lucasbazzani/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucasbazzani/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.lucasbazzani.workshopmongo.resources;
 
+import com.lucasbazzani.workshopmongo.domain.Post;
 import com.lucasbazzani.workshopmongo.domain.User;
 import com.lucasbazzani.workshopmongo.dto.UserDTO;
 import com.lucasbazzani.workshopmongo.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
