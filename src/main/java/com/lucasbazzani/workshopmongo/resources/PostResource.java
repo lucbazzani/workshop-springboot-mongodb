b08a554b184c4282717b07689526043b7357e3c7
lucbazzani <lu.bazzani@hotmail.com>
2021-10-24 18:28:00 -0300

    Find post by title with query method

--- src/main/java/com/lucasbazzani/workshopmongo/resources/PostResource.java
+++ src/main/java/com/lucasbazzani/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.lucasbazzani.workshopmongo.resources;
 
 import com.lucasbazzani.workshopmongo.domain.Post;
+import com.lucasbazzani.workshopmongo.resources.util.URL;
 import com.lucasbazzani.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
