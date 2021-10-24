5f4ae1b1a8d6bc030dadabd8e407f4bfbb2a3ae9
lucbazzani <lu.bazzani@hotmail.com>
2021-10-24 14:25:27 -0300

    Post User

--- src/main/java/com/lucasbazzani/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasbazzani/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lucasbazzani.workshopmongo.services;
 
 import com.lucasbazzani.workshopmongo.domain.User;
+import com.lucasbazzani.workshopmongo.dto.UserDTO;
 import com.lucasbazzani.workshopmongo.repository.UserRepository;
 import com.lucasbazzani.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
