c71dd155d0ea1980f1fe7de11069a081d45fc6d0
lucbazzani <lu.bazzani@hotmail.com>
2021-10-24 17:52:22 -0300

    Find post by id

---
+++ src/main/java/com/lucasbazzani/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.lucasbazzani.workshopmongo.services;
+
+import com.lucasbazzani.workshopmongo.domain.Post;
+import com.lucasbazzani.workshopmongo.domain.User;
+import com.lucasbazzani.workshopmongo.repository.PostRepository;
+import com.lucasbazzani.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
